e00c4af10d9ac3f2840069334f9a6a4727f125fa
leiweii <156084273+leiweii@users.noreply.github.com>
2024-11-12 15:27:21 +0100

    Update rest.php

--- rest/rest.php
+++ rest/rest.php
@@ -109,9 +109,8 @@ abstract class Rest {
             }
         }
         else {
-            if (get_magic_quotes_gpc()) {
                 $data = trim(stripslashes($data));
-            }
+    
             $data = strip_tags($data);
             $clean_input = trim($data);
         }
